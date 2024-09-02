42df6d03c658f11bbf19bb763c013ee3b863b430
mattc-27 <108201614+mattc-27@users.noreply.github.com>
2024-09-02 16:36:40 -0600

    updates 09022401 (#4)

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -16,6 +16,7 @@ export default function CurrentWeather() {
 
     const [showHourly, setShowHourly] = useState(null);
     const [showAlerts, setShowAlerts] = useState(null);
+    const [mobile, setMobile] = useState(false);
 
     function handleHourly() {
         if (!showHourly) {
@@ -32,9 +33,15 @@ export default function CurrentWeather() {
 
     const [width, setWidth] = useState(window.innerWidth);
 
+
+
     useEffect(() => {
         const handleResize = () => {
-            setWidth(window.innerWidth);
+            if (window.innerWidth < 740) {
+                setMobile(true)
+            } else {
+                setMobile(false)
+            }
         };
 
         window.addEventListener('resize', handleResize);
@@ -43,7 +50,7 @@ export default function CurrentWeather() {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
-    
+
     useEffect(() => {
         console.log(width)
     }, [setWidth])
@@ -53,105 +60,100 @@ export default function CurrentWeather() {
 
     return (
         <div className='container'>
-        <div className='current-weather'>
-            <div className='main-weather conditions-bg'>
-
-
-                <div className='conditions-current'>
-
-                    <div className='conditions-main'>
-                        <div className='conditions-main-top'>
-
-                            <p id='currentTemp'>{currentConditions.temp_f.toFixed()}°F</p>
-                            <p id='currentText'>{currentConditions.condition.text}</p>
-                        </div>
-                        <div className='current-top-col'>
-                            <div className='wx-icon'>{currentConditions.formatIcon.value}</div>
-                        </div>
-                        
-                    </div>
-
-                    <div className='conditions-location'>
-
-                        <div className='location-text'>
-                            <CiLocationOn size={80} id='locationIcon' />
-                            <h2>{currentConditions.name}, {currentConditions.region}</h2>
-                        </div>
-                        {currentConditions.airQuality ?
-                            <div className='air-quality'>
-                                <AirQuality data={currentConditions.airQuality} />
-                            </div>
-                            : null}
-                    </div>
-
-                </div>
-
-
-                <div className='conditions-current'>
-                    <div className='card-border'>
-                        <div className='conditions-row'>
-                            <div className='conditions-item'>
-                                <FaTemperatureArrowUp size={iconSize - 5} className='conditions-item-icon' />
-                                <p>{currentConditions.forecastday[0].day.maxtemp_f}°F</p>
-                            </div>
-                            <div className='conditions-item'>
-                                <FaTemperatureArrowDown size={iconSize - 5} className='conditions-item-icon' />
-                                <p>{currentConditions.forecastday[0].day.mintemp_f}°F</p>
-                            </div>
-                        </div>
-                        <div className='conditions-row'>
-                            <div className='conditions-item'>
-                                <LiaWindSolid size={iconSize} className='conditions-item-icon' />
-                                <p>{currentConditions.wind_mph}mph, {currentConditions.wind_dir}</p>
-                            </div>
-                            <div className='conditions-item'>
-                                <LiaCloudSunSolid size={iconSize} className='conditions-item-icon' />
-                                <p>{currentConditions.cloud}%</p>
-                            </div>
-                        </div>
-                        <div className='conditions-row'>
-                            <div className='conditions-item'>
-                                <WiFog size={iconSize} className='conditions-item-icon' />
-                                <p>{currentConditions.vis_miles}mi</p>
-                            </div>
-                            <div className='conditions-item'>
-                                <CiDroplet size={iconSize} className='conditions-item-icon' />
-                                <p>{currentConditions.humidity}%</p>
-                            </div>
-                        </div>
-                        <div className='conditions-row'>
-                            {currentConditions.sun.map((item) => (
-                                <div className='conditions-item'>
-                                    <>
-                                        {item.name === "sunrise" ? <BsSunrise size={iconSize} className='conditions-item-icon' />
-                                            : <BsSunset size={iconSize} className='conditions-item-icon' />
-                                        }
-                                    </>
-                                    <>
-                                        <p> {item.time}</p>
-                                    </>
+            <div className='current-weather'>
+                <div className='main-weather conditions-bg'>
+                    <div className='conditions-current'>
+                        <div className='conditions-main'>
+                            <div className='conditions-main-top'>
+                                <div className='conditions-text'>
+                                    <p id='currentTemp'>{currentConditions.temp_f.toFixed()}°F</p>
                                 </div>
-                            ))}
+                                <div className='conditions-text'>
+                                    <p id='currentText'>{currentConditions.condition.text}</p>
+                                </div>
+                            </div>
+                            <div className='current-top-col'>
+                                <div className='wx-icon'>
+                                    <div className='icon-card'>
+                                        {mobile ? currentConditions.formatIcon.daily : currentConditions.formatIcon.value}
+                                    </div>
+                                </div>
+                                {currentConditions.airQuality ?
+                                    <div className='air-quality'>
+                                        <AirQuality data={currentConditions.airQuality} />
+                                    </div>
+                                    : null}
+                            </div>
+                        </div>
+                        <div className='conditions-location'>
+                            <div className='location-text'>
+                                <h2>{currentConditions.name}, {currentConditions.region}</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='conditions-current'>
+                        <div className='card-border'>
+                            <div className='conditions-row'>
+                                <div className='conditions-item'>
+                                    <FaTemperatureArrowUp size={iconSize - 5} className='conditions-item-icon' />
+                                    <p>{currentConditions.forecastday[0].day.maxtemp_f}°F</p>
+                                </div>
+                                <div className='conditions-item'>
+                                    <FaTemperatureArrowDown size={iconSize - 5} className='conditions-item-icon' />
+                                    <p>{currentConditions.forecastday[0].day.mintemp_f}°F</p>
+                                </div>
+                            </div>
+                            <div className='conditions-row'>
+                                <div className='conditions-item'>
+                                    <LiaWindSolid size={iconSize} className='conditions-item-icon' />
+                                    <p>{currentConditions.wind_mph}mph, {currentConditions.wind_dir}</p>
+                                </div>
+                                <div className='conditions-item'>
+                                    <LiaCloudSunSolid size={iconSize} className='conditions-item-icon' />
+                                    <p>{currentConditions.cloud}%</p>
+                                </div>
+                            </div>
+                            <div className='conditions-row'>
+                                <div className='conditions-item'>
+                                    <WiFog size={iconSize} className='conditions-item-icon' />
+                                    <p>{currentConditions.vis_miles}mi</p>
+                                </div>
+                                <div className='conditions-item'>
+                                    <CiDroplet size={iconSize} className='conditions-item-icon' />
+                                    <p>{currentConditions.humidity}%</p>
+                                </div>
+                            </div>
+                            <div className='conditions-row'>
+                                {currentConditions.sun.map((item) => (
+                                    <div className='conditions-item'>
+                                        <>
+                                            {item.name === "sunrise" ? <BsSunrise size={iconSize} className='conditions-item-icon' />
+                                                : <BsSunset size={iconSize} className='conditions-item-icon' />
+                                            }
+                                        </>
+                                        <>
+                                            <p> {item.time}</p>
+                                        </>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className='img-container' style={{
+                    backgroundImage: `${background}`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                }}></div>
             </div>
-
-            <div className='img-container' style={{
-                backgroundImage: `${background}`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-            }}></div>
-
+            <div className='hourly-row'>
+                {
+                    currentConditions.forecastday[0].hour.filter((_, index) => index % 4 === 0).map(hour => (
+                        <Hourly hour={hour} />
+                    ))
+                }
+            </div>
         </div>
-        <div className='hourly-row'>
-            {
-                currentConditions.forecastday[0].hour.filter((_, index) => index % 4 === 0).map(hour => (
-                    <Hourly hour={hour} />
-                ))
-            }
-        </div>
-    </div>
     );
 }
